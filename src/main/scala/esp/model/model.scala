a11c873e48162f8d4a62712d03ff41b75054f1d0
Lukasz Golebiewski <lukasz.golebiewski@blstream.com>
2015-07-29 12:42:45 +0200

    Format esp.model package

--- src/main/scala/esp/model/model.scala
+++ src/main/scala/esp/model/model.scala
@@ -9,17 +9,14 @@ package object model {
 
 package model {
 
-case class User(firstName: String, lastName: String, phone: Option[String], email: Option[String])
+  case class User(firstName: String, lastName: String, phone: Option[String], email: Option[String])
+  case class Account(number: AccountNumber, name: String, balance: BigDecimal)
+  case class Transaction(from: Option[AccountNumber], to: AccountNumber, amount: BigDecimal)
 
-case class Account(number: AccountNumber, name: String, balance: BigDecimal)
-
-case class Transaction(from: Option[AccountNumber], to: AccountNumber, amount: BigDecimal)
-
-object Bank {
-  val Account = esp.model.Account(UUID.randomUUID().toString, "main-bank-account", 1024)
-
-  def transferFee: BigDecimal => BigDecimal = _ * 0.03
-}
+  object Bank {
+    val Account = esp.model.Account(UUID.randomUUID().toString, "main-bank-account", 1024)
+    def transferFee: BigDecimal => BigDecimal = _ * 0.03
+  }
 
 }
 
