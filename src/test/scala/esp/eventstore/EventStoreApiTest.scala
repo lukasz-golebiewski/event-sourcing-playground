f3d1e8164175bd6df8d51e5ecc626a019c262dd7
Lukasz Golebiewski <lukasz.golebiewski@blstream.com>
2015-09-15 10:59:21 +0200

    Rename EsApiTest

--- src/test/scala/esp/eventstore/EventStoreApiTest.scala
+++ src/test/scala/esp/eventstore/EventStoreApiTest.scala
@@ -3,4 +3,4 @@ package esp.eventstore
 import esp.PendingIfUnimplemented
 import esp.model.ApiTest
 
-class EsApiTest extends ApiTest with EsApi with PendingIfUnimplemented
+class EventStoreApiTest extends ApiTest with EsApi with PendingIfUnimplemented
